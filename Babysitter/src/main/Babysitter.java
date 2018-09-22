7f7fda0e8187e344b01e80f8f08f8a3c16a6a54d
James Ha <hajamesjw@gmail.com>
2018-09-22 01:17:51 -0400

    Testing that only hours from 0 through 23 are considered valid

--- Babysitter/src/main/Babysitter.java
+++ Babysitter/src/main/Babysitter.java
@@ -7,6 +7,20 @@ public class Babysitter {
 	public static final int LATEST_END_TIME = 4;
 
 	public Babysitter(int startTime, int bedtime, int endTime) {
-		
+		//One day consists of hours 0 through 23
+		validateTimesAreNotNegative(startTime, bedtime, endTime);
+		validateTimesAreNotOverTwentyThree(startTime, bedtime, endTime);
+	}
+
+	private void validateTimesAreNotOverTwentyThree(int startTime, int bedtime, int endTime) {
+		if (startTime > 23 || bedtime > 23 || endTime > 23) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateTimesAreNotNegative(int startTime, int bedtime, int endTime) {
+		if (startTime < 0 || bedtime < 0 || endTime < 0) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
--- Babysitter/src/test/TestBabysitter.java
+++ Babysitter/src/test/TestBabysitter.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import main.Babysitter;
@@ -9,5 +10,19 @@ public class TestBabysitter {
 	@Test
 	public void testValidStartTimeBedtimeAndEndTimeAreAccepted() {
 		new Babysitter(Babysitter.EARLIEST_START_TIME, Babysitter.LATEST_BEDTIME, Babysitter.LATEST_END_TIME);
+	}
+	
+	@Test
+	public void testInvalidTimeThatIsLessThanZeroIsRejected() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Babysitter(-1, Babysitter.LATEST_BEDTIME, Babysitter.LATEST_END_TIME);
+		});
+	}
+	
+	@Test
+	public void testInvalidTimeThatIsGreaterThanTwnetyThreeIsRejected() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Babysitter(Integer.MAX_VALUE, Babysitter.LATEST_BEDTIME, Babysitter.LATEST_END_TIME);
+		});
 	}
 }
