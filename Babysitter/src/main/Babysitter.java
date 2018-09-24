48ec0574f37eef877b48f09bf6de4ef9dfd02b38
James Ha <hajamesjw@gmail.com>
2018-09-24 01:04:24 -0400

    Testing that the bababysitter does not accept invalid start time, bedtime, or end time. Refactored the changes.

--- Babysitter/src/main/Babysitter.java
+++ Babysitter/src/main/Babysitter.java
@@ -2,11 +2,19 @@ package main;
 
 public class Babysitter {
 
-	public static final int EARLIEST_START_TIME = 17;
-	public static final int LATEST_BEDTIME = 23;
-	public static final int LATEST_END_TIME = 4;
+	public static final int START_TIME_EARLIEST = 17;
+	public static final int START_TIME_LATEST = 23;
+	public static final int BEDTIME_EARLIEST = 17;
+	public static final int BEDTIME_LATEST = 23;
+	public static final int END_TIME_EARLIEST = START_TIME_EARLIEST;
+	public static final int END_TIME_LATEST = 4;
 	
-	public static final String INVALID_TIME_RANGES = "Valid time inputs must be from 1 to 24.";
+	public static final String INVALID_TIME_RANGES_MESSAGE = "Valid time inputs must be from 1 to 24.";
+	public static final String INVALID_EARLY_START_TIME_MESSAGE = "Start time cannot be before " + START_TIME_EARLIEST + ".";
+	public static final String INVALID_LATE_START_TIME_MESSAGE = "Start time cannot be after " + START_TIME_LATEST + ".";
+	public static final String INVALID_EARLY_BEDTIME_MESSAGE = "Bedtime cannot be before " + BEDTIME_EARLIEST + ".";
+	public static final String INVALID_LATE_BEDTIME_MESSAGE = "Bedtime cannot be after " + BEDTIME_LATEST + ".";
+	public static final String INVALID_END_TIME_MESSAGE = "End time must be between " + END_TIME_EARLIEST + " and " + END_TIME_LATEST + ".";
 	
 	public Babysitter(int startTime, int bedtime, int endTime) {
 		validateTimes(startTime, bedtime, endTime);
@@ -14,11 +22,30 @@ public class Babysitter {
 
 	private void validateTimes(int startTime, int bedtime, int endTime) {
 		validateTimesAreFromOneToTwentyFour(startTime, bedtime, endTime);
+		validateTimesAgainstEarliestAndLatestAllowedTimes(startTime, bedtime, endTime);
 	}
 
 	private void validateTimesAreFromOneToTwentyFour(int startTime, int bedtime, int endTime) {
 		if (startTime < 1 || bedtime < 1 || endTime < 1 || startTime > 24 || bedtime > 24 || endTime > 24) {
-			throw new IllegalArgumentException(INVALID_TIME_RANGES);
+			throw new IllegalArgumentException(INVALID_TIME_RANGES_MESSAGE);
+		}
+	}
+	
+	private void validateTimesAgainstEarliestAndLatestAllowedTimes(int startTime, int bedtime, int endTime) {
+		if (startTime < START_TIME_EARLIEST) {
+			throw new IllegalArgumentException(INVALID_EARLY_START_TIME_MESSAGE);
+		} 
+		if (startTime > START_TIME_LATEST) {
+			throw new IllegalArgumentException(INVALID_LATE_START_TIME_MESSAGE);
+		}
+		if (bedtime < BEDTIME_EARLIEST) {
+			throw new IllegalArgumentException(INVALID_EARLY_BEDTIME_MESSAGE);
+		}
+		if (bedtime > BEDTIME_LATEST) {
+			throw new IllegalArgumentException(INVALID_LATE_BEDTIME_MESSAGE);
+		}
+		if (endTime < END_TIME_EARLIEST && endTime > END_TIME_LATEST) {
+			throw new IllegalArgumentException(INVALID_END_TIME_MESSAGE);
 		}
 	}
 }
--- Babysitter/src/test/TestBabysitter.java
+++ Babysitter/src/test/TestBabysitter.java
@@ -11,24 +11,68 @@ public class TestBabysitter {
 
 	@Test
 	public void testValidStartTimeBedtimeAndEndTimeAreAccepted() {
-		new Babysitter(Babysitter.EARLIEST_START_TIME, Babysitter.LATEST_BEDTIME, Babysitter.LATEST_END_TIME);
+		new Babysitter(Babysitter.START_TIME_EARLIEST, Babysitter.BEDTIME_LATEST, Babysitter.END_TIME_LATEST);
 	}
 	
 	@Test
 	public void testInvalidTimeLessThanOneIsRejected() {
-		checkErrorMessagesGivenInvalidTimes(Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE, Babysitter.INVALID_TIME_RANGES);
+		verifyExceptionMessageGivenIllegalArguments(Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE, Babysitter.INVALID_TIME_RANGES_MESSAGE);
 	}
 	
 	@Test
 	public void testInvalidTimeGreaterThanTwnetyFourIsRejected() {
-		checkErrorMessagesGivenInvalidTimes(Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Babysitter.INVALID_TIME_RANGES);
+		verifyExceptionMessageGivenIllegalArguments(Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Babysitter.INVALID_TIME_RANGES_MESSAGE);
 	}
 	
-	private void checkErrorMessagesGivenInvalidTimes(int startTime, int bedtime, int endTime, String expectedExceptionMessage) {
+	@Test
+	public void testInvalidStartTimeBeforeEarliestStartTimeIsRejected() {
+		verifyExceptionMessageGivenIllegalArguments(oneHourBefore(Babysitter.START_TIME_EARLIEST), Babysitter.BEDTIME_LATEST, Babysitter.END_TIME_LATEST, Babysitter.INVALID_EARLY_START_TIME_MESSAGE);
+	}
+	
+	@Test
+	public void testInvalidStartTimeAfterLatestStartTimeIsRejected() {
+		verifyExceptionMessageGivenIllegalArguments(oneHourAfter(Babysitter.START_TIME_LATEST), Babysitter.BEDTIME_LATEST, Babysitter.END_TIME_LATEST, Babysitter.INVALID_LATE_START_TIME_MESSAGE);
+	}
+	
+	@Test
+	public void testInvalidBedtimeBeforeEarliestBedtimeIsRejected() {
+		verifyExceptionMessageGivenIllegalArguments(Babysitter.START_TIME_EARLIEST, oneHourBefore(Babysitter.BEDTIME_EARLIEST), Babysitter.END_TIME_LATEST, Babysitter.INVALID_EARLY_BEDTIME_MESSAGE);
+	}
+	
+	@Test
+	public void testInvalidBedtimeAfterLatestBedtimeIsRejected() {
+		verifyExceptionMessageGivenIllegalArguments(Babysitter.START_TIME_EARLIEST, oneHourAfter(Babysitter.BEDTIME_LATEST), Babysitter.END_TIME_LATEST, Babysitter.INVALID_LATE_BEDTIME_MESSAGE);
+	}
+	
+	@Test
+	public void testInvalidEndTimeBeforeEarliestEndTimeIsRejected() {
+		verifyExceptionMessageGivenIllegalArguments(Babysitter.START_TIME_EARLIEST, Babysitter.BEDTIME_LATEST, oneHourBefore(Babysitter.END_TIME_EARLIEST), Babysitter.INVALID_END_TIME_MESSAGE);
+	}
+	
+	@Test
+	public void testInvalidEndTimeAfterLatestEndTimeIsRejected() {
+		verifyExceptionMessageGivenIllegalArguments(Babysitter.START_TIME_EARLIEST, Babysitter.BEDTIME_LATEST, oneHourAfter(Babysitter.END_TIME_LATEST), Babysitter.INVALID_END_TIME_MESSAGE);
+	}
+	
+	private void verifyExceptionMessageGivenIllegalArguments(int startTime, int bedtime, int endTime, String expectedExceptionMessage) {
 		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
 			new Babysitter(startTime, bedtime, endTime);
 		});
 		
 		assertEquals(expectedExceptionMessage, exception.getMessage());
+	}
+	
+	private int oneHourBefore(int time) {
+		if (time == 1) { 
+			return 24;
+		}
+		return Integer.valueOf(time) - 1;
+	}
+	
+	private int oneHourAfter(int time) {
+		if (time == 24) {
+			return 1;
+		}
+		return Integer.valueOf(time) + 1;
 	}
 }
